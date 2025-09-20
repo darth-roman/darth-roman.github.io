c94c4b0bebc799ac71b36fa13b7f01470def5c0f
Chouchane Abderrahmane <chouchane.louai20@gmail.com>
2025-09-20 13:21:43 +0100

    <feat>: whats left count is shown on randoms

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const codeblock = document.querySelector("code")
 const spans = document.querySelectorAll("code span")
+let RANDOMS = 0
 
 for(let i=0; i<spans.length; i++){
     spans[i].style.animation = `fadeIn 500ms ${0.2*i}s 1 forwards`
@@ -17,8 +18,12 @@ async function fetchSocials() {
     return results.socials
 }
 
+
+
 async function appendRandoms(container, renderCount){
     const randoms = await fetchRandoms()
+
+    RANDOMS = randoms.length
 
     if(renderCount>randoms.length) renderCount = randoms.length
 
@@ -37,7 +42,22 @@ async function appendRandoms(container, renderCount){
             <a href="${random.link}" target="_blank">Click me</a>
         </div>`
     });
+    
 }
+
+function whatsLeftComponent(total, sliceIndex){
+    const whatsLeft = document.querySelector("#whats-left")
+    let whatsLeftCount = total - sliceIndex
+
+    if(whatsLeftCount < 0) {
+        whatsLeft.textContent = `(${0}) left`
+        return whatsLeft
+    }
+    whatsLeft.textContent = `(${whatsLeftCount}) left`
+    
+    return whatsLeft
+}
+
 
 async function appendSocials(container){
     const socials = await fetchSocials()
@@ -52,19 +72,31 @@ async function appendSocials(container){
     });
 }
 
-window.addEventListener("load", async function(){
+window.addEventListener("DOMContentLoaded", async function(){
+
+    
     
     if(window.location.pathname === "/pages/randoms.html"){
         const laodMore = this.document.querySelector("#loadmore")
         laodMore.attributeStyleMap.set("display", "block")
+
         const randomsContainter = this.document.querySelector(".randoms-container")
+        
         let defaultSLice = 3
+
         laodMore.addEventListener("click", async function(){
             defaultSLice += 3
             await appendRandoms(randomsContainter, defaultSLice)
+            whatsLeftComponent(RANDOMS, defaultSLice)
+
+            if(RANDOMS-defaultSLice<0) {
+                whatsLeftComponent(RANDOMS, defaultSLice)
+                return
+            }
             return
         })
         await appendRandoms(randomsContainter, defaultSLice)
+        whatsLeftComponent(RANDOMS, defaultSLice)
     }
 
     if(window.location.pathname === "/pages/connect.html"){
